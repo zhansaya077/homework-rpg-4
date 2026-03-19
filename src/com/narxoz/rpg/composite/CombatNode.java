1b8bf59ad7b96e4b95b4fc9e2fb43be3f9ce3d12
zhansaya077 <zhansaya.bekbulat@narxoz.kz>
2026-03-19 20:50:31 +0500

    Update CombatNode.java
    Removed getHealth and getChildren methods from CombatNode interface.

--- src/com/narxoz/rpg/composite/CombatNode.java
+++ src/com/narxoz/rpg/composite/CombatNode.java
@@ -4,10 +4,8 @@ import java.util.List;
 
 public interface CombatNode {
     String getName();
-    int getHealth();
     int getAttackPower();
     void takeDamage(int amount);
     boolean isAlive();
-    List<CombatNode> getChildren();
     void printTree(String indent);
 }
